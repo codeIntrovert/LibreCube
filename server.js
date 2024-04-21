2d899b21e48d066725730de6bcf413f394e216b8
Hasan <9131411681hasan@gmail.com>
2024-04-21 12:57:26 +0530

    Added project assets

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const nunjucks = require('nunjucks');
+
+const app = express();
+
+// Configure Nunjucks
+nunjucks.configure('views', {
+    autoescape: true,
+    express: app
+});
+
+// Set the view engine to use Nunjucks
+app.set('view engine', 'njk');
+
+// Define routes
+app.get('/', (req, res) => {
+    res.render('index', { title: 'My Express App', message: 'Hello, Nunjucks!' });
+});
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
